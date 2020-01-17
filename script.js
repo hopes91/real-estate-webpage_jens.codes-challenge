979ad568207b249891854147f4e4d1a78fa731b7
hopes91 <nadya.me91@gmail.com>
2020-01-17 17:01:23 +0300

    fix the dot's indication

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const slider = document.querySelector('.slider');
 const arrowLeft = document.querySelector('.arrows .arrow-left');
 const arrowRight = document.querySelector('.arrows .arrow-right');
 const dotsContainer = document.querySelector('.dots');
-const dots = document.querySelectorAll('.dot');
 const slides = document.querySelectorAll('.slide');
 // const socialMediaIconsContainer = document.querySelector('.social-media-icons');
 
 const handleAnimation = () => {
   setTimeout(() => {
     slider.style.right = '0';
-    showSlides();
+    createDots();
+    showSlide();
   }, 0);
 
   setTimeout(() => {
@@ -106,44 +106,37 @@ const handleArrows = index => {
     mainSlide = 0 :
     mainSlide += index;
 
-  showSlides();
+  showSlide();
 };
 
-const showSlides = () => {
+const showSlide = () => {
   slides.forEach(slide => slide.style.display = 'none');
 
   slides.forEach((slide, index) => {
     if (index === mainSlide) {
       slide.style.display = 'block';
 
-      createDots();
       detectCurrentSlide();
     }
   });
 };
 
 const detectCurrentSlide = () => {
+  const dots = document.querySelectorAll('.dot');
+
   dots.forEach(dot => {
     dot.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-    dot.style.borderColor = 'rgba(0, 0, 0, 0.7)';
   });
 
   dots.forEach((dot, index) => {
     if (index === mainSlide) {
-      dot.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-      dot.style.borderColor = 'rgba(0, 0, 0, 1)';
+      dot.style.backgroundColor = 'rgba(127, 131, 140, 0.5)';
     }
   });
 };
 
-const createDots = () => {
-  dotsContainer.innerHTML = '';
-
-  slides.forEach(slide => {
-    const dot = document.createElement('span');
-    dot.setAttribute('class', 'dot');
-    dotsContainer.appendChild(dot);
-  });
+const moveSlides = () => {
+  setInterval(() => handleArrows(1), 5000);
 };
 
 const indicateHoverOverTitle = () => {
